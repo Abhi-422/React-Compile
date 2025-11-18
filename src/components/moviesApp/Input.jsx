15dcb902bdadf9d35566df16525c539d962a6195
Abhi-422 <abhishekpatange422@gmail.com>
2025-11-18 17:17:48 +0530

    ok

--- src/components/moviesApp/Input.jsx
+++ src/components/moviesApp/Input.jsx
@@ -5,7 +5,7 @@ const Input = ({ value, onChange, placeholder = "Search for Movie" }) => {
       value={value}
       onChange={onChange}
       placeholder={placeholder}
-      className="w-full px-4 py-2 bg-transparent text-gray-800 dark:text-white placeholder-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+      className="w-full px-4 py-2 bg-transparent text-black  placeholder-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
     />
   );
 };
